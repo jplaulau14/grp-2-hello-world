6984a676e14d8cebf95c55ae714084d3916fa8f6
jplaulau14 <code.patricklaurel@gmail.com>
2022-09-17 16:01:24 +0800

    fix

--- main.py
+++ main.py
@@ -4,10 +4,6 @@ st.title('Hexasquad best squad 🤩')
 st.write('Pats was here')
 st.write('*Karen is lurking*')
 st.write('Sof was here (basic)')
-<<<<<<< HEAD
-st.write('Chiara was here')
-=======
 st.write('Enrico was here')
 st.write('Karla was here')
->>>>>>> 1a636c651366b454698660e5d5c74a60c4c6ca29
 st.write('Chiara was here')
